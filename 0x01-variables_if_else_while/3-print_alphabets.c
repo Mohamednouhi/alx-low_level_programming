5985edc2c03841f6150e8f63c2b7f831ed3bad70
mohamed nouhi <mohamednoouuhi@gmail.com>
2023-06-16 15:20:43 -0700

    alphabet lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlin.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * Return: always 0 (success)
+ */
+
+int main(void)
+
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	putchar(low)
+
+	for (low = 'A'; low <= 'z'; low++)
+	putchar(low);
+	putchar('\n');
+
+	return(0);

16b492aa77e7363409821871d1c942060eff43a2
mohamed nouhi <mohamednoouuhi@gmail.com>
2023-06-15 17:45:03 -0700

    fput

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <unlstd.h>
 /**
  * main - program prin dora
  * Return: 1 (success)
-*/
+ */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout):
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }

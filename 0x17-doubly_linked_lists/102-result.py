ca320f2b4e931cb858e810e20c8f07c3d1f1b893
mohamed nouhi <you@example.com>
2023-10-12 15:48:14 -0700

    all

--- 0x17-doubly_linked_lists/102-result.py
+++ 0x17-doubly_linked_lists/102-result.py
@@ -2,8 +2,8 @@
 
 largest_palindrome = 0
 
-for i in range(100, 1000):
-    for j in range(100, 1000):
+for j in range(100, 1000):
+    for i in range(100, 1000):
         product = i * j
         if str(product) == str(product) [::-1] and product > largest_palindrome:
             largest_palindrone = product
